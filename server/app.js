5baf8357300f2f95d5e74511241499e400f464ea
Sa1m0n31 <sajmon0031@gmail.com>
2021-04-30 11:31:41 +0200

    Redirect from http to https in app.js

--- server/app.js
+++ server/app.js
@@ -13,6 +13,19 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// /* Redirect http to https */
+app.enable('trust proxy');
+app.use (function (req, res, next) {
+    if (req.secure) {
+        // request was via https, so do no special handling
+        next();
+    } else {
+        // request was via http, so redirect to https
+        res.redirect('https://' + req.headers.host + req.url);
+    }
+});
+
+
 /* Serve static React page */
 app.use(express.static(path.join(__dirname, '../client/build')));
 app.get("*", (req, res) => {
